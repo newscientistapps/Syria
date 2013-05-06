6217174cab201b3fe87e26b0a43d875191045846
paldhous <peter@peteraldhous.com>
2013-05-05 19:33:58 -0700

    chart formatted

--- js/maps.js
+++ js/maps.js
@@ -1,9 +1,14 @@
 var all = L.mapbox.tileLayer('newscientist26102012.map-djlmnumv'),
-	a1 = L.mapbox.tileLayer('newscientist26102012.map-djlmnumv');
-            
+	Q12011 = L.mapbox.tileLayer('newscientist26102012.map-djlmnumv'),
+	Q22011 = L.mapbox.tileLayer('newscientist26102012.map-djlmnumv'),
+	Q32011 = L.mapbox.tileLayer('newscientist26102012.map-djlmnumv'),
+	Q42011 = L.mapbox.tileLayer('newscientist26102012.map-djlmnumv'),
+	Q12012 = L.mapbox.tileLayer('newscientist26102012.map-djlmnumv'),
+	Q22012 = L.mapbox.tileLayer('newscientist26102012.map-djlmnumv');
+              
 var map = L.map('map', {
     center: new L.LatLng(35, 36.5),
-    zoom: 7,
+    zoom: 7, 
     maxZoom: 9,
     minZoom: 5,
     layers: [all]
@@ -11,7 +16,12 @@ var map = L.map('map', {
 
 var baseMaps = {
     "All": all,
-    "2013": a1
+    "Q1 2011": Q12011,
+	"Q2 2011": Q22011,
+	"Q3 2011": Q32011,
+	"Q4 2011": Q42011,
+	"Q1 2012": Q12012,
+	"Q2 2012": Q22012
 };
 
 var layersControl = new L.Control.Layers(baseMaps, null, {collapsed: false});
