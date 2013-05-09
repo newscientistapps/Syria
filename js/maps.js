bb7fd14fbfdda8ff46ab105ad64e4f34445b54cf
paldhous <peter@peteraldhous.com>
2013-05-09 03:05:45 -0700

    new data in chart

--- js/maps.js
+++ js/maps.js
@@ -4,8 +4,11 @@ var all = L.mapbox.tileLayer('newscientist26102012.map-djlmnumv'),
 	Q32011 = L.mapbox.tileLayer('newscientist26102012.map-djlmnumv'),
 	Q42011 = L.mapbox.tileLayer('newscientist26102012.map-djlmnumv'),
 	Q12012 = L.mapbox.tileLayer('newscientist26102012.map-djlmnumv'),
-	Q22012 = L.mapbox.tileLayer('newscientist26102012.map-djlmnumv');
-              
+	Q22012 = L.mapbox.tileLayer('newscientist26102012.map-djlmnumv'),
+	Q32012 = L.mapbox.tileLayer('newscientist26102012.map-djlmnumv'),
+	Q42012 = L.mapbox.tileLayer('newscientist26102012.map-djlmnumv'),
+	Q12013 = L.mapbox.tileLayer('newscientist26102012.map-djlmnumv');
+	
 var map = L.map('map', {
     center: new L.LatLng(35, 36.5),
     zoom: 7, 
@@ -21,7 +24,10 @@ var baseMaps = {
 	"Q3 2011": Q32011,
 	"Q4 2011": Q42011,
 	"Q1 2012": Q12012,
-	"Q2 2012": Q22012
+	"Q2 2012": Q22012,
+	"Q3 2012": Q32012,
+	"Q4 2012": Q42012,
+	"Q1 2013": Q12013
 };
 
 var layersControl = new L.Control.Layers(baseMaps, null, {collapsed: false});
